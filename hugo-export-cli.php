603d6832a8f42c60cde2d18417d81eb8c588dee5
Paul Lan <lanshunfang@gmail.com>
2019-04-07 12:09:06 +0800

    [#76] CLI: Should exit if the configured temp folder is invalid

--- hugo-export-cli.php
+++ hugo-export-cli.php
@@ -5,6 +5,10 @@
  * Usage:
  *
  *     $ php hugo-export-cli.php > my-hugo-files.zip
+ * 
+ * Usage: (With configured tmp folder if the avaiable space in system /tmp is insufficent for migration)
+ * 
+ *     $ php hugo-export-cli.php /mnt/tmp-folder
  *
  * Must be run in the wordpress-to-hugo-exporter/ directory.
  *
@@ -14,8 +18,21 @@ include_once "../../../wp-load.php";
 include_once "../../../wp-admin/includes/file.php";
 require_once "hugo-export.php";
 
+$tmpFolder = $argv[1];
+
 $je = new Hugo_Export();
-if (isset($argv[1]) && 'null' !== strtolower($argv[1]) && is_dir($argv[1])) {
-    $je->setTempDir($argv[1]);
+
+if (isset($tmpFolder)) {
+    if ('null' !== strtolower($tmpFolder) && is_dir($tmpFolder)) {
+        echo "[INFO] Start to export data to configured folder $tmpFolder";
+        $je->setTempDir($tmpFolder);
+    } else {
+        echo "[WARN] Passed TEMP folder $tmpFolder is not a valid folder. Remove the argument or create it as a folder before contiune";
+        exit(1);
+    }
+    
+} else {
+    echo "[INFO] tmp folder not found, use default. You could invoke php hugo-export-cli.php with an extra argument as the temporary folder path if needful.";
 }
+
 $je->export();
